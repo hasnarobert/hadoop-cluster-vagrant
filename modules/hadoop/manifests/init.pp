b9c1810ed44243a510860a9280a3c03e48330a2b
Robert Hasna <hasna.robert@gmail.com>
2013-10-19 16:53:37 +0300

    Added haddop conf files.

--- modules/hadoop/manifests/init.pp
+++ modules/hadoop/manifests/init.pp
@@ -14,4 +14,45 @@ class hadoop {
         creates => "${hadoop_home}-1.2.1",
         require => Exec["download_hadoop"]
     }
+
+    file { "${hadoop_home}-1.2.1/conf/slaves":
+        source => "puppet:///modules/hadoop/slaves",
+        mode => 644,
+        owner => root,
+        group => root,
+        require => Exec["unpack_hadoop"]
+    }
+
+    file { "${hadoop_home}-1.2.1/conf/masters":
+        source => "puppet:///modules/hadoop/masters",
+        mode => 644,
+        owner => root,
+        group => root,
+        require => Exec["unpack_hadoop"]
+    }
+
+    file { "${hadoop_home}-1.2.1/conf/core-site.xml":
+        source => "puppet:///modules/hadoop/core-site.xml",
+        mode => 644,
+        owner => root,
+        group => root,
+        require => Exec["unpack_hadoop"]
+    }
+
+    file { "${hadoop_home}-1.2.1/conf/mapred-site.xml":
+        source => "puppet:///modules/hadoop/mapred-site.xml",
+        mode => 644,
+        owner => root,
+        group => root,
+        require => Exec["unpack_hadoop"]
+    }
+
+    file { "${hadoop_home}-1.2.1/conf/hdfs-site.xml":
+        source => "puppet:///modules/hadoop/hdfs-site.xml",
+        mode => 644,
+        owner => root,
+        group => root,
+        require => Exec["unpack_hadoop"]
+    }
+
 }
