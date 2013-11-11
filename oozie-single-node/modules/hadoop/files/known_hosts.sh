5fde41016d10f4af5f4bb02e0ee741c67e24d097
Robert Hasna <hasna.robert@gmail.com>
2013-11-11 23:22:44 +0200

    Removed bogus stuff.

--- oozie-single-node/modules/hadoop/files/known_hosts.sh
+++
@@ -1,4 +0,0 @@
-ssh-keyscan -H 192.168.1.10 >> /root/.ssh/known_hosts
-ssh-keyscan -H master >> /root/.ssh/known_hosts
-ssh-keyscan -H localhost >> /root/.ssh/known_hosts
-ssh-keyscan -H 127.0.0.1 >> /root/.ssh/known_hosts
